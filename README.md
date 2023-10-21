0419480d93fcd050004f094714f19a64bb918c04
self esteem <selfesteem@gmail.com>
2023-10-21 09:28:03 +0900

    refac use popcorn

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# Getting Started with Create React App
-## https://usepopcorn.netlify.app/
+# react intermediate complex course
+
+## The first App
+### https://usepopcorn.netlify.app/
+* 적당한 크기의 component 는 ?
+* 너무 크거나 너무 작으면 ?
+* 논리적으로 분리, 재사용 성, 복잡성을 줄이고...
+ 
+1. Logical separation of content/layout
+2. re-usability
+3. responsibilities / complexity
+4. Personal coding style
 
---
+++ src/usePopcorn/SelectedList.js
@@ -0,0 +1,70 @@
+const average = (arr) =>
+  arr.reduce((acc, cur) => (acc + cur) ,0) / arr.length;
+
+function SelectedList({isOpen2, onOpen2, watched }) {
+
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched.map((movie) => movie.userRating));
+  const avgRuntime = average(watched.map((movie) => movie.runtime));
+
+  return (
+    <div className="box">
+      <button
+        className="btn-toggle"
+        onClick={onOpen2}
+      >
+        {isOpen2 ? "–" : "+"}
+      </button>
+      {isOpen2 && (
+        <>
+          <div className="summary">
+            <h2>Movies you watched</h2>
+            <div>
+              <p>
+                <span>#️⃣</span>
+                <span>{watched.length} movies</span>
+              </p>
+              <p>
+                <span>⭐️</span>
+                <span>{avgImdbRating}</span>
+              </p>
+              <p>
+                <span>🌟</span>
+                <span>{avgUserRating}</span>
+              </p>
+              <p>
+                <span>⏳</span>
+                <span>{avgRuntime} min</span>
+              </p>
+            </div>
+          </div>
+
+          <ul className="list">
+            {watched.map((movie) => (
+              <li key={movie.imdbID}>
+                <img src={movie.Poster} alt={`${movie.Title} poster`} />
+                <h3>{movie.Title}</h3>
+                <div>
+                  <p>
+                    <span>⭐️</span>
+                    <span>{movie.imdbRating}</span>
+                  </p>
+                  <p>
+                    <span>🌟</span>
+                    <span>{movie.userRating}</span>
+                  </p>
+                  <p>
+                    <span>⏳</span>
+                    <span>{movie.runtime} min</span>
+                  </p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default SelectedList
